1f1af1500f21d8cf2dbcdd9b0c0dfcc361c3178f
audwinoyong <audwinoyong@live.com>
2018-09-05 02:03:22 +1000

    Add welcome page, change title

--- src/Main.elm
+++ src/Main.elm
@@ -1,40 +1,77 @@
-import Html exposing (Html, button, div, text)
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
 
+
+-- APP
+
+
+main : Program Never Model Msg
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 
+
 -- MODEL
 
-type alias Model = Int
+
+type Page
+  = WelcomePage
+  | QuestionPage
+
+
+type alias Model =
+  { currentPage: Page }
+
 
 model : Model
 model =
-  0
+  { currentPage = WelcomePage }
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+
+type Msg
+  = StartTrivia
+
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    StartTrivia ->
+      { model | currentPage = QuestionPage }
 
-    Decrement ->
-      model - 1
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
+  div
+    [ class "container" ]
+    [ contentView model ]
+
+
+contentView : Model -> Html Msg
+contentView model =
+  case model.currentPage of
+    WelcomePage ->
+      welcomeView
+
+    QuestionPage ->
+      welcomeView
+
+
+welcomeView : Html Msg
+welcomeView =
+  div
+    []
+    [ h1 [] [ text "Trivia Game" ]
+    , button [ onClick StartTrivia ] [ text "START" ]
     ]
